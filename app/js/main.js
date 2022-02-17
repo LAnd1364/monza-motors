e61a7e1cdc4b24bdf3c99f8edd557d1a56ffbdec
LAnd1364 <and.lazarenko@gmail.com>
2022-02-17 08:35:05 +0200

    add functional

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,39 @@
 let selects = document.querySelectorAll('.form-select');
-let filters = document.querySelectorAll('.filters .form-select');
+let filters = document.querySelectorAll('.offers__select');
+let filterYear = document.querySelector('.offers__year');
+let filterMake = document.querySelector('.offers__make');
+let filterModel = document.querySelector('.offers__model');
+let filterTrim = document.querySelector('.offers__trim');
+let filtersBtn = document.querySelector('.offers__btn');
 let checkbox = document.querySelector('.offers__checkbox');
+
+let allCars = document.querySelectorAll('.car');
+let allCarsYear = document.querySelectorAll('.car__year');
+let allCarsMake = document.querySelectorAll('.car .card-title');
+let allCarsTrim = document.querySelectorAll('.car__trim');
+
+let selectedYear = filterYear.value;
+let selectedMake = filterMake.value;
+let selectedModel = filterModel.value;
+let selectedTrim = filterTrim.value;
+
+filtersBtn.innerHTML = `${allCars.length} cars`;
+
+filtersBtn.addEventListener('click', filterCars)
+
 let subscrWrap = document.querySelector('.subscribe__input-wrap');
 let subscrInput = document.querySelector('.subscribe__input');
 let subscrInputMsg = document.querySelector('.subscribe__input-message');
 let subscrBtn = document.querySelector('.subscribe__btn');
 
+
 document.addEventListener('click', deactSelects1);
 document.addEventListener('contextmenu', deactSelects2);
 document.addEventListener('visibilitychange', deactSelects2);
 
-checkbox.addEventListener('click', switcher);
-
 filters.forEach(el => activate(el));
+
+checkbox.addEventListener('click', switcher);
 
 subscrInput.addEventListener('focus', styleInputOnFocus);
 subscrInput.addEventListener('blur', (e) => e.target.placeholder = 'Enter your email');
@@ -39,6 +60,49 @@ function activate(el) {
     this.classList.add('active')
   })
 }
+
+function filterCars() {
+  let carNumber = 0;  
+  
+  allCars.forEach(el => el.classList.remove('hidden'));
+  
+  if (selectedYear !== '0') {
+    allCarsYear.forEach(el => el.innerHTML !== selectedYear ? el.closest('.car').classList.add('hidden') : 0);
+  }
+
+  if (selectedMake !== '0') {
+    allCarsMake.forEach(el => el.innerHTML.split(' ')[0] !== selectedMake ? el.closest('.car').classList.add('hidden') : 0);
+  }
+
+  if (selectedModel !== '0') {
+    allCarsMake.forEach(function(el) {
+      let val = el.innerHTML.split(' ').slice(1).join(' ');
+      console.log(val);
+      val !== selectedModel ? el.closest('.car').classList.add('hidden') : 0
+    }
+  )}
+
+  if (selectedTrim !== '0') {
+    allCarsTrim.forEach(el => el.innerHTML !== selectedTrim ? el.closest('.car').classList.add('hidden') : 0);
+  }
+
+  allCars.forEach(el => !el.classList.contains('hidden') ? carNumber++ : 0);
+  filtersBtn.innerHTML = `${carNumber} cars`;
+}
+
+filterYear.addEventListener('change', function() {
+  selectedYear = this.value;
+})
+filterMake.addEventListener('change', function() {
+  selectedMake = this.value;
+})
+filterModel.addEventListener('change', function() {
+  selectedModel = this.value;
+})
+
+filterTrim.addEventListener('change', function() {
+selectedTrim = this.value;
+})
 
 function switcher() {
   let label = this.nextElementSibling;
